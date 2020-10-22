524539aeb5a941bdd25fe8b84f4352739766da92
Jeff Lile <jeffplile@gmail.com>
2020-10-22 16:42:54 -0400

    Removed reload after alert confirmation. Added a form reset in the Javascript.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// This sets the probability of any given horse number to win.
 let horseArray = [
     "3",
     "3",
@@ -10,12 +11,14 @@ let horseArray = [
     "1",
     "1"
     ];
-    
+
+// The winning horse is chosen from the array at random.
 let winningHorseNumber = horseArray[Math.floor(Math.random()*horseArray.length)];
 
+// This logs the winner in the console during the testing phase.
 console.log("Winning Horse:", winningHorseNumber)
 
-
+// This action determines whether the user entered a valid input for their horse number bet.
 betButton.addEventListener('click', () => {
     let x;
     x = document.getElementById("horse-choice").value;
@@ -26,14 +29,17 @@ betButton.addEventListener('click', () => {
     }                        
 });
 
+// This action displays an alert if the horse picked was correct or incorrect.
 betButton.addEventListener('click', () => {
     let x;
     x = document.getElementById("horse-choice").value;
     if (x === winningHorseNumber) {
-        !alert('You picked the correct horse!'); location.reload();
+        !alert('You picked the correct horse!'); 
+        document.getElementById("horseForm").reset();
     } else if (x >= 1 && x <= 3 && x !== winningHorseNumber) {
-        !alert('Sorry! You picked a loser.'); location.reload();
+        !alert('Sorry! You picked a loser.'); 
+        document.getElementById("horseForm").reset();
     } else {
-        //do nothing
+        // Do nothing.
     }                    
 });
