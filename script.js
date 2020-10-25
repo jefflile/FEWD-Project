f51cc744ac4b36737840924cd1b6dcc5850416ab
Jeff Lile <jeffplile@gmail.com>
2020-10-25 11:47:04 -0400

    Added initial scripting of hamburger menu

--- script.js
+++ script.js
@@ -60,3 +60,20 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+
+
+
+
+
+
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function menuFunction() {
+    var x = document.getElementById("menuLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
