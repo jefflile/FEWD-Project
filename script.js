27b76182efc9e364b0ece09acdf99792f93ecf67
Jeff Lile <jeffplile@gmail.com>
2020-10-25 11:15:29 -0400

    Added expanders and content for the section describing the horses

--- script.js
+++ script.js
@@ -43,3 +43,20 @@ betButton.addEventListener('click', () => {
         // Do nothing.
     }                    
 });
+
+
+// This controls the expander action.
+var acc = document.getElementsByClassName("expander");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
