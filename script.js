acd8838ddc8df1fed5243e7faed633ac554d0811
Jeff Lile <jeffplile@gmail.com>
2020-10-27 10:48:15 -0400

    Moved winning horse JS into the betFunction on click for repeated uses

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-// This sets the probability of any given horse number to win.
-let horseArray = [
-    "3",
-    "3",
-    "3",
-    "3",
-    "3",
-    "2",
-    "2",
-    "2",
-    "1",
-    "1"
-    ];
-
-// The winning horse is chosen from the array at random.
-let winningHorseNumber = horseArray[Math.floor(Math.random()*horseArray.length)];
-
-// This logs the winner in the console during the testing phase.
-console.log("Winning Horse:", winningHorseNumber)
-
 // This action determines whether the user entered a valid input for their horse number bet.
 betButton.addEventListener('click', () => {
     let x;
@@ -32,8 +12,24 @@ betButton.addEventListener('click', () => {
 // This action displays an alert if the horse picked was correct or incorrect.
 betButton.addEventListener('click', () => {
     let x;
+    let horseArray;
     x = document.getElementById("horse-choice").value;
-    if (x === winningHorseNumber) {
+    horseArray = [
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "2",
+      "2",
+      "2",
+      "1",
+      "1"
+    ]
+    // The winning horse is chosen from the array at random.
+    let winningHorseNumber = horseArray[Math.floor(Math.random()*horseArray.length)];
+
+    if (x >= 1 && x <= 3 && x === winningHorseNumber) {
         !alert('You picked the correct horse!'); 
         document.getElementById("horseForm").reset();
     } else if (x >= 1 && x <= 3 && x !== winningHorseNumber) {
